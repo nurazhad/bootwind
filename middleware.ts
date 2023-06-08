a71a6ba6b0e540180432893291ae6cc1e7745c5d
Nendy Azhad Nurahman <azhadnurahmann@outlook.com>
2023-06-08 09:59:08 +0700

    setup supabase

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ import type { Database } from '@/types_db'
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient<Database>({ req, res })
+
   await supabase.auth.getSession()
   return res
 }
